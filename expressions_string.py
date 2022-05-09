f93b2d1d90296e6f930f7675ead034559f49b8f6
dcsepan <csepi1995@gmail.com>
2022-05-09 16:42:09 +0200

    Ini

---
+++ expressions_string.py
@@ -0,0 +1,23 @@
+print("Hello")
+
+print("Hello" + "David")
+name = "John"
+
+print("Hello" + name)
+
+print(2 * name)
+
+index = 5
+print(type(index))
+print("Ez az" + str(index) + ".könyvtár.")
+#str(számváltozó) átalakítja az integert, így összelehet vonni stringgel változók között.
+
+print("Ez az", index , ".könyvtár", sep="")
+
+print(f"Ez az {index} lemez.") #uazt csinálja mint a fenit 2. megoldás
+
+digit = "12"
+print(digit)
+
+digit += "x"
+print(digit)
---
+++ input_demo.py
@@ -0,0 +1,16 @@
+#Feladat 1
+#name = input("Mi a neved?")
+
+#print("Hello" + " " + name + "!")
+
+#Feladat 2: Mi a kedvenc számod? 6, favourite_number, A kedvenc számod duplája: 12
+# answer  = input("Mi a kedvenc számod?")
+# favourite_number = int(answer)
+
+favourite_number = int(input("Mi a kedvenc számod?"))
+print (type(favourite_number))
+print("Kedvenc számod: " + str(favourite_number)) 
+print("A kedvenc számod duplája: " + str(favourite_number * 2) )
+
+#Feladat  3:
+# int("abc")
