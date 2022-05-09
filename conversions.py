ca8056606cbda2b9a248e1a5b4ed7d0c7a06116f
dcsepan <csepi1995@gmail.com>
2022-05-09 16:53:03 +0200

    Ini

---
+++ conversions.py
@@ -0,0 +1,9 @@
+print(float(17))
+print(type(float(17)))
+
+print(int(17.2))
+print(int(17.8)) # konvertálás nem kerekít, levágja a tizedes jegyet
+
+print(float("17.8"))
+
+print(float("17,8".replace(",",".")))
