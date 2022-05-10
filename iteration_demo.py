4913336101dc6a0e7eaa3db572bac8806869e752
dcsepan <csepi1995@gmail.com>
2022-05-10 10:31:00 +0200

    Ininew

--- iteration_demo.py
+++ iteration_demo.py
@@ -4,15 +4,14 @@
 #for i in range (n):
 #    print("David") #(indent=behúzás)előtte lehet tab vagy space, de csak az egyiket lehet használni
 
-#print(i)
-#for i in range (5):
-#    print(i, "David")
+for i in range (10):
+    print(i, "David")
 
-#for c in "hellopython".upper():
-#    print(c)
+for c in "hellopython".upper():
+    print(c)
 
-#for i in range (2 * 4):
-#    print(i)
+for i in range (2 * 4):
+    print(i)
 
 for e in [3, 9, 11, 2, 8]:
     print(e)
