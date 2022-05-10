809b27f931875e3e276cad709e381c06081272e0
dcsepan <csepi1995@gmail.com>
2022-05-10 11:09:57 +0200

    Ciklus

---
+++ logical_demo.py
@@ -0,0 +1,44 @@
+#Logika kifejezés: olyan kifejezés, melynek az eredménye bool típusu
+
+has_two_eyes = True
+print(has_two_eyes)
+print(type(has_two_eyes))
+
+#Egyenlőség vizsgálat == -jel
+print(5 == 5)
+# =-jel értékadás 
+
+print(6 == 5)
+number = 8
+print(number == 8)
+print(number)
+
+print(number == 22)
+print(number)
+
+name = "John Doe"
+print(name == "John Doe")
+print(name == "Jack Doe")
+
+print(5 > 10)
+print( 5 < 10)
+print(5 < 5)
+print(5 <= 5)
+
+#NOT Operátor
+print (not True)
+print (not False)
+print(not 8 > 10) #Előbb 8>10 elvégzi, majd a not operátor jön 
+
+#OR Operátor, ha egyik teljesül akkor True 
+print(True or False) #True az eredmény
+print(False or False) #False az eredmény
+print(5 > 2 or 10 < 2) #True a kettő eredményét megnézi, és utána True+False= True 
+
+#And, ha mindkettő teljesül akkor True 
+print(True and True)
+print(False and True)
+print(False and False)
+
+
+
---
+++ while_demo.py
@@ -0,0 +1,6 @@
+# Elől tesztelő ciklus
+i = 0 # i: kulcsszó, 0: logikai kifejezés  
+while i < 5 : # Fej
+    print(i)  # Törzs, itt is be kell húzni, több utasítás is leet 
+    i = i + 1 # Törzs, += uaz. mint i = i + 1 
+print("end")
